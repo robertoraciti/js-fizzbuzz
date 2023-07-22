64c9bce4708a3d5f7910dc66418147fa44c57b86
robertoraciti <rob.raciti97@gmail.com>
2023-07-22 13:45:46 +0200

    Complete ex

---
+++ README.md
@@ -0,0 +1,14 @@
+# TRACCIA
+
+Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz". Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz". Prima di partire a scrivere codice poniamoci qualche domanda: Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare che possiamo usare?
+
+## ISTRUZIONI
+
+- Prendo la lista
+- Creo 100 elementi nella lista con un ciclo FOR
+- li metto all'interno della lista precedentemente creata
+- **SE** l'elemento della lista è multiplo di 3 scriverò Fizz.
+- **INVECE SE** l'elemento della lista è multiplo di 5 scriverò Buzz.
+- **INVECE SE** l'elemento della lista è multiplo di 3 scriverò FizzBuzz.
+- **INVECE** l'elemento della lista non è multiplo dei numeri citati prima conteggia il numero dell'elemento.
+- Stilizzo e formatto gli elementi della lista
--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-// Scrivi un programma che stampi in console i numeri da 1 a 100,
-// ma che per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
-// Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Come faccio a sapere se un numero è divisibile per un altro?
-// Abbiamo visto qualcosa di particolare che possiamo usare?
+const myList = document.getElementById("my-list");
+console.log(myList);
+
+for (let i = 1; i <= 100; i++) {
+  const listItem = document.createElement("li");
+  console.log(listItem);
+  listItem.innerHTML += ``;
+  listItem.style.width = "100px";
+  listItem.style.height = "100px";
+  listItem.style.display = "flex";
+  listItem.style.alignItems = "center";
+  listItem.style.justifyContent = "center";
+  listItem.style.margin = "10px";
+  listItem.style.fontWeight = "bold";
+
+  myList.append(listItem);
+  if (i % 15 == 0) {
+    listItem.innerHTML += "FizzBuzz";
+    listItem.style.backgroundColor = "#f0466f";
+  } else if (i % 5 == 0) {
+    listItem.innerHTML += "Buzz";
+    listItem.style.backgroundColor = "#ffd166";
+  } else if (i % 3 == 0) {
+    listItem.innerHTML += "Fizz";
+    listItem.style.backgroundColor = "#0cd6a1";
+  } else {
+    listItem.innerHTML += ` ${i}`;
+    listItem.style.backgroundColor = "#1389b2";
+  }
+}
